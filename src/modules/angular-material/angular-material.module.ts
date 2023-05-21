5f331cabb834cbd3e87363eb2881b04c05771cea
Davis Sylvester <davis@sylvesterllc.com>
2023-05-20 23:13:19 -0500

    Initial

---
+++ src/modules/angular-material/angular-material.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatStepperModule,
+    MatTabsModule,
+  ],
+})
+export class AngularMaterialModule { }
